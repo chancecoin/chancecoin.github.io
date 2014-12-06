247c25f69daac759199d8096b102a6571ed3c227
chancecoin <chancecoin@chancecoin.com>
2014-12-06 17:29:24 -0600

    update

--- js/lite.js
+++ js/lite.js
@@ -1,6 +1,5 @@
 //TODO: make balances update when send txs happen
 //TODO: make chancecoin.com load from github
-//TODO: make ajax for writing bets table more snappy (add rows instead of rewriting whole thing)
 
 //CONFIG
 var FEE_ADDRESS = "1CHACHAGuuxTr8Yo9b9SQmUGLg9X5iSeKX";
@@ -29,19 +28,33 @@ var HOME = "https://chancecoin.github.io";
 var UPDATING = false;
 var BALANCES = null;
 var LOG = [];
+var CONTENT = null;
 
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
-$(window).on('load', function () {
-    $('.selectpicker').selectpicker({
-        'selectedText': 'cat'
-    });
+$(function(){
+  $("[data-hide]").on("click", function(){
+    $($(this).attr("data-hide")).hide();
+  });
 });
 
 $(function(){
-    $("[data-hide]").on("click", function(){
-        $($(this).attr("data-hide")).hide();
-    });
+  $("[data-template]").on("click", function(){
+    var page = $(this).attr("data-template");
+    if (page == "home") {
+      if (CONTENT) {
+        $("#content").html(CONTENT);
+      }
+      CONTENT = null;
+    } else {
+      var url = "http://www.corsproxy.com/chancecoin.github.io/templates/" + page;
+      var result = download(url);
+      if (!CONTENT) {
+        CONTENT = $("#content").html();
+      }
+      $("#content").html(result);
+    }
+  });
 });
 
 $(document).ready(function() {
@@ -1011,13 +1024,13 @@ function hideMessage() {
   $("#ajax_error").hide();
 }
 
-function enableForm(formName) {
-  $(formName+" input").prop("disabled", true);
-  $(formName+" button").prop("disabled", true);
-}
 function disableForm(formName) {
-  $(formName+" input").prop("disabled", false);
-  $(formName+" button").prop("disabled", false);
+  $("#"+formName+" input").prop("disabled", true);
+  $("#"+formName+" button").prop("disabled", true);
+}
+function enableForm(formName) {
+  $("#"+formName+" input").prop("disabled", false);
+  $("#"+formName+" button").prop("disabled", false);
 }
 
 function getBTCPrice() {
@@ -1160,6 +1173,7 @@ function getBTCBlockHeight(hash) {
         CACHE_getBTCBlockHeight[hash] = blockHeight;
       });
     }
+    CACHE_getBTCBlockHeight[hash] = blockHeight;
     return blockHeight;
   }
 }
